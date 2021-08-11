69163f81f685d1c7880a04fb2082476478eb074c
Shawna Akiona <shawna.akiona@gmail.com>
2021-08-11 10:15:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(objective = '*'){
+    return function(special = 'hard worker'){ 
+        return `You are ${objective}${special}${objective}!`
+    }
+}
